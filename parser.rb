1fae9ca0c495ea32b2da3b9f33338e91714a44a4
cybdoom <cybdoom@alexs-MacBook-Pro.local>
2015-11-16 19:51:54 +0200

    Finished core implementation

--- parser.rb
+++ parser.rb
@@ -11,8 +11,41 @@ class Parser
   end
 
   def stats_for_user user_index
-    @ftp_client.chdir "user#{user_index}"
-    @ftp_client.chdir 'playlists'
+    @user_index = user_index
+    @ftp_client.chdir File.join('root', "user#{@user_index}", 'playlists')
+    @meta_info = {}
+    @sizes = {}
+    begin
+      @ftp_client.gettextfile('playlists.txt') do |line|
+        name, size = line.split(' ').flatten
+        @meta_info[name] = size.to_i
+        @sizes[name] = 0
+      end
+    rescue Exception => e
+      puts 'Error while reading playlists meta info'
+      exit 2
+    end
+    get_sizes
+    generate_stats
+  end
 
+  private
+
+  def get_sizes
+    @sizes = {}
+    @ftp_client.list.each do |str|
+      next unless str =~ /\Ad/
+      chunks = str.split(' ')
+      @sizes[chunks.last] = chunks[4].to_i
+    end
+  end
+
+  def generate_stats
+    @stats = {}
+    puts @meta_info
+    @sizes.each {|name, size| @stats[name] = (size * 100.to_f/@meta_info[name]).round(2) }
+    @stats = {
+      "user#{@user_index}" => @stats
+    }
   end
 end
